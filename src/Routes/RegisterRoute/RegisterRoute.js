d4b2a79c92e5a05bc765e748aa41e1cfe4f6463f
Sam Walker <jack.nasal@gmail.com>
2019-10-21 15:29:43 -0400

    finished the login and register routes

--- src/Routes/RegisterRoute/RegisterRoute.js
+++ src/Routes/RegisterRoute/RegisterRoute.js
@@ -1,16 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import LoginForm from '../../components/Loginform/LoginForm'
+import AuthApiService from '../../services/auth-api-service'
+import TokenService from '../../services/token-service'
 
 import './registerRoute.css'
 
 class RegisterRoute extends React.Component {
 
-    render() {
-        return(
-            <section className = "register">
-                Register
-            </section>
-        ) 
-    }
+  state = {
+    error: null
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const user_name = event.target.user_name.value
+    const password = event.target.password.value
+    AuthApiService.postUser({user_name, password})
+      .then(response => {
+        console.log('authToken is '+response.authToken)
+        TokenService.saveAuthToken(response.authToken)
+        this.props.history.push('/')
+      })
+      .catch(res => this.setState({ error: res.error }))
+  }
+
+  render() {
+      return(
+          <section className = "login">
+              <h2>Sign Up</h2>
+              <LoginForm onSubmit={this.handleSubmit} buttonText='Sign up' error={this.state.error}/>
+              <p>Already a member? <Link to='/login'>Log in</Link></p>
+          </section>
+      )
+  }
 }
 
 export default RegisterRoute
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'http://localhost:8080/api',
     TOKEN_KEY: 'recipe-client-auth-token',
   }
   
