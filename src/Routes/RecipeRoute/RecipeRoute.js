6f44ed600f381ad1b33c89c09190663f05d74609
Fabian Lema <fabianlemac@gmail.com>
2019-10-22 12:00:31 -0500

    recipe list page

---
+++ src/Routes/RecipeRoute/RecipeRoute.js
@@ -0,0 +1,68 @@
+import React from 'react'
+//import { Link } from 'react-router-dom'
+
+import Recipe from '../../components/Recipe/Recipe'
+import RecipeContext from '../../contexts/RecipeContext'
+import RecipeApiService from '../../services/recipe-api-service'
+
+class RecipeRoute extends React.Component {
+
+  state = {
+    recipeList: [],
+    error: null
+  }
+
+  setError = (error) => {
+    this.setState({
+      error
+    })
+  }
+
+  clearError = () => {
+    this.setState({
+      error: null
+    })
+  }
+
+  setRecipeList = (recipeList) => {
+    this.setState({
+      recipeList
+    })
+  }
+
+  removeRecipe = (idProduct) => {
+    this.setState({
+        recipeList: this.state.recipeList.filter((item, index) => item.id !== idProduct)
+    });
+  }
+
+  getAllRecipes = () => {
+    RecipeApiService.getAll()
+      .then(this.setRecipeList)
+  }
+
+  delete = (idRecipe) => {
+    RecipeApiService.delete(idRecipe)
+      .then(() => this.removeRecipe(idRecipe))
+      .catch(this.setError)
+  }
+
+  render() {
+    const value = {
+      recipeList: this.state.recipeList,
+      error: this.state.error,
+      setError: this.setError,
+      clearError: this.clearError,
+      getAllRecipes: this.getAllRecipes,
+      delete: this.delete,
+    }
+
+    return(
+      <RecipeContext.Provider value={value}>
+        <Recipe />
+      </RecipeContext.Provider>
+    )
+  }
+}
+
+export default RecipeRoute
---
+++ src/services/recipe-api-service.js
@@ -0,0 +1,95 @@
+import config from '../config'
+import TokenService from './token-service';
+
+const RecipeApiService = {
+  getAll() {
+      return fetch(`${config.API_ENDPOINT}/recipes`, {
+        headers: {
+          "Authorization": `bearer ${TokenService.getAuthToken()}`,
+        },
+      })
+        .then(res =>
+          (!res.ok)
+            ? res.json().then(e => Promise.reject(e))
+            : res.json()
+        )
+    },
+
+  getById(id) {
+        return fetch(`${config.API_ENDPOINT}/recipes/${id}`, {
+          headers: {
+            "Authorization": `bearer ${TokenService.getAuthToken()}`,
+          },
+        })
+          .then(res =>
+            (!res.ok)
+              ? res.json().then(e => Promise.reject(e))
+              : res.json()
+          )
+      },
+
+  saveNew(obj) {
+    return fetch(`${config.API_ENDPOINT}/recipes`, {
+      method: 'POST',
+      headers: {
+        "Authorization": `bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+
+  saveExisting(id, obj) {
+    return fetch(`${config.API_ENDPOINT}/recipes/${id}`, {
+      method: 'PATCH',
+      headers: {
+        "Authorization": `bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : Promise.resolve('OK')
+      )
+  },
+
+  delete(id) {
+    return fetch(`${config.API_ENDPOINT}/recipes/${id}`, {
+      method: 'DELETE',
+      headers: {
+        "Authorization": `bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : Promise.resolve('OK')
+      )
+  },
+
+  deleteWithResult(id) {
+    return fetch(`${config.API_ENDPOINT}/recipes/${id}`, {
+      method: 'DELETE',
+      headers: {
+        "Authorization": `bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  }
+
+}
+
+export default RecipeApiService;
