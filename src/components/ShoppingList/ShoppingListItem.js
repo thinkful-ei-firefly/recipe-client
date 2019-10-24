de7a06d1743ac182e6b67ec0e9db4a96e40e1366
Sam Walker <jack.nasal@gmail.com>
2019-10-24 11:54:30 -0400

    implemented crossing off without API support

--- src/components/ShoppingList/ShoppingListItem.js
+++ src/components/ShoppingList/ShoppingListItem.js
@@ -5,15 +5,26 @@ import { Link } from 'react-router-dom'
 class RecipeItem extends React.Component {
   static contextType = ShoppingListRoute
 
+  state = {
+    crossed: this.props.recipe.crossed
+  }
+
+  crossUncross = () => {
+    this.setState({crossed: !this.state.crossed})
+  }
+
   render() {
     console.log(this.props.recipe);
-    const { id, name } = this.props.recipe
+    const { id, name, amount, unit } = this.props.recipe
+    const { crossed } = this.state
+    const text = crossed ? <strike>{`${amount} ${unit} of ${name}`}</strike> : `${amount} ${unit} of ${name}`
     //const ingredientList = ingregients.map(ingredient => <div>{ingredient}</div>)
     //const instructionList = instructions.map(instruction => <div>{instruction}</div>)
 
       return(
           <section className="recipe">
-              <div>{name}</div>
+              <div>{text}</div>
+              <button onClick={this.crossUncross}>{crossed ? 'Uncross' : 'Cross off'}</button>
               <button onClick={e => this.context.delete(id)}>Remove</button>
               <Link
                   to="/editrecipe"
