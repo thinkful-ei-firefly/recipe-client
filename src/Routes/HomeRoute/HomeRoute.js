054a05599786e3dae3ee1d2cae771d1897ba5193
Sam Walker <jack.nasal@gmail.com>
2019-10-25 15:25:27 -0400

    added Dashboard component to landing page, only loads when logged in

--- src/Routes/HomeRoute/HomeRoute.js
+++ src/Routes/HomeRoute/HomeRoute.js
@@ -1,27 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 import background from '../../images/background1.png'
+import MenuContext from '../../contexts/MenuContext'
+import Dashboard from '../../components/Dashboard/Dashboard'
+import TokenService from '../../services/token-service'
 
 import './homeRoute.css'
 
 class HomeRoute extends React.Component {
 
+  static contextType = MenuContext
+
     render() {
+      const loggedIn = TokenService.hasAuthToken()
         return(
             <div className='landing'>
               <section className = "home">
                 <img src={background} alt='background' className='background'/>
                 <h2>Welcome to GoodMeal! </h2>
                 <div className='subtitle'>
-                  <p>Create and save your favorite recipes</p>
-                  <p>Create a grocery list</p>
-                  <p>Create delicious meals</p>
+                  <p hidden={loggedIn}>Create and save your favorite recipes</p>
+                  <p hidden={loggedIn}>Create a grocery list</p>
+                  <p hidden={loggedIn}>Create delicious meals</p>
                 </div>
               </section>
               
-              <Link to="/login" className="button">Login</Link>
-              <Link to="/register"className="button">Register</Link>
-              
+              {
+                loggedIn ? <Dashboard /> :
+                <div>
+                  <Link to="/login" className="button">Login</Link>
+                  <Link to="/register"className="button">Register</Link> 
+                </div> 
+              }
             </div>
         )
     }
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Dashboard.css'
+
+const Dashboard = () => {
+  return (
+    <div className='Dashboard'>
+      <section className='PublicRecipes'>
+        <div>
+          View and search recipes added by other users. If you find one you like, add it to your recipe list and make
+          any modifications or substitutions you want.
+        </div>
+        <Link to='/placeholder'>
+          <button>All Recipes</button>
+        </Link>
+      </section>
+      <section className='MyRecipes'>
+        <div>
+          Curate your personal list or recipes. Write new ones, modify old ones, and search your recipes by title,
+          ingredients, description, cuisine, or time to make.
+        </div>
+        <Link to='/recipes'>
+          <button>My Recipes</button>
+        </Link>
+      </section>
+      <section className='ShoppingList'>
+        <div>
+          Once you know what you want to cook, keep track of the ingredients you'll need with a shopping list you can
+          take with you on your next excursion.
+        </div>
+        <Link to='/shoppinglist'>
+          <button>My Shopping</button>
+        </Link>
+      </section>
+      <section className='MyIngredients'>
+        <div>
+          Instead of trying to remember every ingredient sitting in your pantry, keep track of them here to make it that
+          much easier to figure out what meals you can mkae from what you already own.
+        </div>
+        <Link to='/ingredients'>
+          <button>My Pantry</button>
+        </Link>
+      </section>
+    </div>
+  )
+}
+
+export default Dashboard
