423ec0d1d350989f7199b9698e07cf3ff33d4175
Hector Valles <hmvalles75@yahoo.com>
2019-10-28 12:41:12 -0700

    links working

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -15,14 +15,16 @@ const Dashboard = () => {
             <img src= {recipe2} alt='icon'></img> 
           </Link>
         </div>
+        <Link to='/publicrecipes'>
           <p>
           View and search recipes added by other users. If you find one you like, add it to your recipe list and make
           any modifications or substitutions you want.
           </p>
+          </Link>
           <div className='card_image'>
         </div>
           <div className='card_title title_white'>
-            <h3>All Recipes</h3>
+          <h3><Link className='link' to='/publicrecipes'>All Recipes</Link></h3>
           </div>
       </section>
 
@@ -32,14 +34,16 @@ const Dashboard = () => {
       <img src= {recipe2} alt='icon'></img> 
         </Link>
         </div>
+        <Link to='/recipes'>
         <p>
           Curate your personal list or recipes. Write new ones, modify old ones, and search your recipes by title,
           ingredients, description, cuisine, or time to make.
         </p>
+        </Link>
         <div className='card_image'>
         </div>
         <div className='card_title title_white'>
-          <h3>My Recipes</h3>
+        <h3><Link className='link' to='/recipes'>My Recipes</Link></h3>
         </div>
       </section>
 
@@ -49,14 +53,15 @@ const Dashboard = () => {
       <img src= {recipe2} alt='icon'></img> 
         </Link>
         </div>
+        <Link to='/shoppinglist'>
         <p>
           Once you know what you want to cook, keep track of the ingredients you'll need with a shopping list you can
           take with you on your next excursion.
-        </p>
+        </p></Link>
       <div className='card_image'>
         </div>
         <div className='card_title title_white'>
-          <h3>My Shopping</h3>
+        <h3><Link className='link' to='/shoppinglist'>My Shopping</Link></h3>
         </div>
       </section>
 
@@ -66,15 +71,18 @@ const Dashboard = () => {
       <img src= {recipe2} alt='icon'></img> 
         </Link>
         </div>
+        <Link to='/ingredients'>
         <p>
           Instead of trying to remember every ingredient sitting in your pantry, keep track of them here to make it that
           much easier to figure out what meals you can make from what you already own.
         </p>
+        </Link>
       <div className='card_image'>
         </div>
         <div className='card_title title_white'>
-          <h3>My Pantry</h3>
+        <h3><Link className='link' to='/ingredients'>My Pantry</Link></h3>
         </div> 
+        
       </section>
     </div>
   )
