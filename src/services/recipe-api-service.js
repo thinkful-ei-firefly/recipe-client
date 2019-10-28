f1bd014d91046fe940dd9bf69e339d9ef9a696d9
Fabian Lema <fabianlemac@gmail.com>
2019-10-28 10:28:34 -0500

    clone public recipe

--- src/services/recipe-api-service.js
+++ src/services/recipe-api-service.js
@@ -107,8 +107,23 @@ const RecipeApiService = {
           ? res.json().then(e => Promise.reject(e))
           : res.json()
       )
-  }
+  },
 
+  cloneRecipe(id) {
+    return fetch(`${config.API_ENDPOINT}/clone`, {
+      method: 'POST',
+      headers: {
+        "Authorization": `bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({id}),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
 }
 
 export default RecipeApiService;
