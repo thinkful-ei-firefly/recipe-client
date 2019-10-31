c6142e1f0e1fdfa2eac5c5dba4f9481a1a74c940
rjgates3 <gates.j.richard@gmail.com>
2019-10-31 16:04:48 -0600

    added google login

--- src/config.js
+++ src/config.js
@@ -1,4 +1,13 @@
 export default {
-    API_ENDPOINT: 'https://good-meal-server.herokuapp.com/api',
+    API_ENDPOINT: 'http://localhost:8080/api' || 'https://good-meal-server.herokuapp.com/api',
     TOKEN_KEY: 'recipe-client-auth-token',
+    FirebaseConfig: {
+        apiKey: "AIzaSyBv-WLiOMAh3oBruqOS_yaaZ4PJ1c5PK7s",
+        authDomain: "good-meal-1572448422752.firebaseapp.com",
+        databaseURL: "https://good-meal-1572448422752.firebaseio.com",
+        projectId: "good-meal-1572448422752",
+        storageBucket: "good-meal-1572448422752.appspot.com",
+        messagingSenderId: process.env.Firebase_messagingSenderId,
+        appId: process.env.Firebase_appId
+    }
   }
