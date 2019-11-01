38cb0038c8f15f3a1eceab48d2675c397e434156
Sam Walker <jack.nasal@gmail.com>
2019-11-01 15:20:43 -0400

    fixed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 export default {
-    API_ENDPOINT: 'http://localhost:8080/api',
+    API_ENDPOINT: 'https://good-meal-server.herokuapp.com/api',
     TOKEN_KEY: 'recipe-client-auth-token',
     REACT_APP_GOOGLE_CLIENT_ID: "http://1017027923671-ervk2a58a57uuf055o216oo5aenpjr5r.apps.googleusercontent.com/",
     FirebaseConfig: {
