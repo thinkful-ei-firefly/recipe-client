cbc40a3e8825b32750958f248f89fef51605bef4
rjgates3 <gates.j.richard@gmail.com>
2019-11-04 10:32:53 -0700

    updated context spacing

--- src/contexts/PublicRecipeContext.js
+++ src/contexts/PublicRecipeContext.js
@@ -6,177 +6,203 @@ import TokenService from '../services/token-service'
 import { Link } from 'react-router-dom'
 
 const PublicRecipeContext = React.createContext({
-    recipe: null,
-    display: '',
-    publicRecipesJSX: [],
+  recipe: null,
+  display: '',
+  publicRecipesJSX: [],
 
-    updateRecipe: () => {},
-    updateDisplay: () => {},
-    updateSearchPublicRecipesBy: () => {},
-    getIngredientsList: () => {},
-    getInstructionsList: () => {},
-    getPublicRecipes: () => {},
-    updatePublicRecipesJSX: () => {},
+  updateRecipe: () => {},
+  updateDisplay: () => {},
+  updateSearchPublicRecipesBy: () => {},
+  getIngredientsList: () => {},
+  getInstructionsList: () => {},
+  getPublicRecipes: () => {},
+  updatePublicRecipesJSX: () => {},
 })
 
 export default PublicRecipeContext
 
 export class PublicRecipeProvider extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        const state = {
-            recipe: {
-                title: '',
-                cuisine: '', 
-                description: '',
-                ingredients: [],
-                instructions: [],
-                time_to_make: '',
-                imageurl: '',
-            },
-            publicRecipes: [],
-            publicRecipesJSX: [],
-            searchPublicRecipesBy: '',
-            redirect: false,
-            display: 'summary'
-        }
-
-        this.state = state;
+    const state = {
+      recipe: {
+        title: '',
+        cuisine: '', 
+        description: '',
+        ingredients: [],
+        instructions: [],
+        time_to_make: '',
+        imageurl: '',
+      },
+      publicRecipes: [],
+      publicRecipesJSX: [],
+      searchPublicRecipesBy: '',
+      redirect: false,
+      display: 'summary'
     }
 
-    updateRecipe = recipe => {
+    this.state = state;
+  }
+
+  updateRecipe = recipe => {
+    this.setState({
+      recipe
+    })
+  }
+
+  updateDisplay = display => {
+    this.setState({
+      display
+    })
+  }
+
+  updateSearchPublicRecipesBy = searchPublicRecipesBy => {
+    this.setState({
+      searchPublicRecipesBy
+    })
+  }
+
+  getIngredientsList = () => {
+    return this.state.recipe.ingredients
+      .map((item, index) => 
+        <li 
+          key={ index } 
+          itemProp="recipeIngredient">
+          { item }
+        </li>
+      )
+  }
+
+  getInstructionsList = () => {
+    return this.state.recipe.instructions
+      .map((step, index) => 
+        <li 
+          key = { index }>
+          { step }
+        </li>
+      )
+  }
+
+  getPublicRecipes = () => {
+    return RecipeApiService.getPublicRecipes()
+      .then(recipes => {
         this.setState({
-            recipe
+          publicRecipes: recipes
         })
+      })
+  }
+
+  searchRecipesBy = (recipeList, term) => {
+    return recipeList
+      .filter(recipe =>
+        Object.values(recipe)
+          .join('')
+          .toLowerCase()
+          .includes(term.toLowerCase())
+      )
+  }
+
+  updatePublicRecipesJSX = () => {
+
+    let recipes = this.state.publicRecipes
+    
+    if(this.state.searchPublicRecipesBy !== ''){
+      recipes = this.searchRecipesBy(
+        recipes,
+        this.state.searchPublicRecipesBy
+      )
     }
 
-    updateDisplay = display => {
-        this.setState({
-            display
-        })
+    recipes = recipes.map(recipe =>
+      <div 
+        key={ recipe.id } 
+        className="cards">
+        <section 
+          className="recipe-card"
+          key={ recipe.id }>
+            <Link to={ '/publicrecipes/' + recipe.id }>
+              <div className = "image">
+                <img
+                  src={ 
+                    "https://good-meal.s3.amazonaws.com/" 
+                    + (recipe.imageurl?recipe.imageurl:"nofound.png") 
+                  }
+                  alt={ recipe.name }>
+                </img>
+              </div>
+          </Link>
+          <Link
+            to={ '/publicrecipes/' + recipe.id }
+            className="name">
+            { recipe.name }
+          </Link>
+          <p 
+            className="description">
+            { recipe.description}
+          </p>
+          <p 
+            style={{
+              marginRight:'15px', 
+              marginTop: '2em', 
+              color: '#b6282b', 
+              marginBottom:'-10px'
+            }}
+            className='rating'>
+            { Number(recipe.rating) 
+              ? `Rating: ${recipe.rating}` 
+              : 'Recipe not yet rated'}
+          </p>
+          { 
+            TokenService.hasAuthToken() 
+              && <div className='recipe-buttons'>
+                <button 
+                  className='remove-recipe' 
+                  type='button' 
+                  onClick = { e => this.cloneRecipe(recipe.id) }>
+                  <i className="far fa-copy" 
+                    style={{
+                      fontSize:'24px', 
+                      marginRight:'10px'
+                    }}>
+                  </i>
+                </button>
+              </div>
+          }
+        </section>
+      </div>
+    )
+    this.setState({
+      publicRecipesJSX: recipes
+    })
+  }
+
+  cloneRecipe = (id) => {
+    RecipeApiService.cloneRecipe(id)
+      .then(recipe => this.setState({redirect: true},
+        () => this.setState({redirect: false})
+      ))
     }
 
-    updateSearchPublicRecipesBy = searchPublicRecipesBy => {
-        this.setState({
-            searchPublicRecipesBy
-        })
+  render() {
+    const value = {
+      recipe: this.state.recipe,
+      display: this.state.display,
+      publicRecipesJSX: this.state.publicRecipesJSX,
+
+      updateRecipe: this.updateRecipe,
+      updateDisplay: this.updateDisplay,
+      updateSearchPublicRecipesBy: this.updateSearchPublicRecipesBy,
+      getIngredientsList: this.getIngredientsList,
+      getInstructionsList: this.getInstructionsList,
+      getPublicRecipes: this.getPublicRecipes,
+      updatePublicRecipesJSX: this.updatePublicRecipesJSX,
     }
 
-    getIngredientsList = () => {
-        return this.state.recipe.ingredients
-            .map((item, index) => 
-                <li key = { index } itemProp = "recipeIngredient">
-                    { item }
-                </li>
-            )
-    }
-
-    getInstructionsList = () => {
-        return this.state.recipe.instructions
-            .map((step, index) => 
-                <li key = { index }>
-                    { step }
-                </li>)
-    }
-
-    getPublicRecipes = () => {
-        return RecipeApiService.getPublicRecipes()
-            .then(recipes => {
-                this.setState({
-                    publicRecipes: recipes
-                })
-            })
-    }
-
-    searchRecipesBy = (recipeList, term) => {
-        return recipeList
-                .filter(recipe =>
-                    Object.values(recipe)
-                        .join('')
-                        .toLowerCase()
-                        .includes(term.toLowerCase())
-
-                )
-    }
-
-    updatePublicRecipesJSX = () => {
-
-        let recipes = this.state.publicRecipes
-        if(this.state.searchPublicRecipesBy !== ''){
-            recipes = this.searchRecipesBy(
-                recipes,
-                this.state.searchPublicRecipesBy
-            )
-        }
-
-        recipes = recipes.map(recipe =>
-            <div key={recipe.id} className='cards'>
-                <section className = "recipe-card"
-                    key = { recipe.id }>
-                        <Link to = { '/publicrecipes/' + recipe.id }>
-                        <div className = "image">
-                        <img
-                            src = { "https://good-meal.s3.amazonaws.com/" + (recipe.imageurl?recipe.imageurl:"nofound.png") }
-                            alt = { recipe.name }
-                        />
-                    </div>
-                    </Link>
-                    <Link
-                        to = { '/publicrecipes/' + recipe.id }
-                        className = "name">
-                        { recipe.name }
-                    </Link>
-                    <p className='description'>{ recipe.description}</p>
-                    <p 
-                      style={{marginRight:'15px', marginTop: '2em', color: '#b6282b', marginBottom:'-10px'}}
-                      className='rating'>{Number(recipe.rating) ? `Rating: ${recipe.rating}` : 'Recipe not yet rated'}</p>
-                    { 
-                        TokenService.hasAuthToken() &&
-                        <div className='recipe-buttons'>
-                            <button 
-                                className='remove-recipe' 
-                                type='button' 
-                                onClick = { e => this.cloneRecipe(recipe.id) }>
-                                <i className="far fa-copy" 
-                                style={{fontSize:'24px', marginRight:'10px'}}></i>
-                            </button>
-                        </div>
-                    }
-                </section>
-            </div>
-        )
-        this.setState({
-            publicRecipesJSX: recipes
-        })
-    }
-
-    cloneRecipe = (id) => {
-        RecipeApiService.cloneRecipe(id)
-          .then(recipe => this.setState({redirect: true},
-            () => this.setState({redirect: false})
-          ))
-      }
-
-    render() {
-        const value = {
-            recipe: this.state.recipe,
-            display: this.state.display,
-            publicRecipesJSX: this.state.publicRecipesJSX,
-
-            updateRecipe: this.updateRecipe,
-            updateDisplay: this.updateDisplay,
-            updateSearchPublicRecipesBy: this.updateSearchPublicRecipesBy,
-            getIngredientsList: this.getIngredientsList,
-            getInstructionsList: this.getInstructionsList,
-            getPublicRecipes: this.getPublicRecipes,
-            updatePublicRecipesJSX: this.updatePublicRecipesJSX,
-        }
-
-        return(
-            <PublicRecipeContext.Provider value = {value}>
-                { this.props.children }
-            </PublicRecipeContext.Provider>
-        )
-    }
+    return(
+      <PublicRecipeContext.Provider value={value}>
+        { this.props.children }
+      </PublicRecipeContext.Provider>
+    )
+  }
 }
