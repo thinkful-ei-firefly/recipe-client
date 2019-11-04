1f37a484d7eec9369dc95caffb04b35be3c5f6c5
Fabian Lema <fabianlemac@gmail.com>
2019-11-04 11:44:13 -0600

    share by email

---
+++ src/components/Sharing/Sharing.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+class Sharing extends React.Component {
+
+  shareFacebook = (id) => {
+    var facebookWindow = window.open('https://www.facebook.com/sharer/sharer.php?u=https://good-meal-client.herokuapp.com/publicrecipes/' + id, 'facebook-popup', 'height=350,width=600');
+    if(facebookWindow.focus) {
+      facebookWindow.focus();
+    }
+    return false;
+  }
+
+  shareTwitter = (id) => {
+    var twitterWindow = window.open('https://twitter.com/share?url=https://good-meal-client.herokuapp.com/publicrecipes/' + id, 'twitter-popup', 'height=350,width=600');
+    if(twitterWindow.focus) {
+      twitterWindow.focus();
+    }
+    return false;
+  }
+
+  shareMail = (e, id, name) => {
+    e.preventDefault()
+    console.log(e.target.email.value);
+    const mailTo = e.target.email.value;
+    const mailContent = `Hello,%0D%0A%0D%0AHow to prepare ${name}:%0D%0A%0D%0ATo view the details please click the following link: https://good-meal-client.herokuapp.com/publicrecipes/${id}`
+    window.open(`mailto:${mailTo}?subject=Check this amazing recipe&body=${mailContent}`);
+  }
+
+  render() {
+    const recipe = this.props.recipe
+    return (
+      <div className='share'>
+        <button type='button' onClick={e => this.shareFacebook(recipe.id)}>Share on Facebook</button>
+        <button type='button' onClick={e => this.shareTwitter(recipe.id)}>Share on Twitter</button>
+        <form onSubmit={e => this.shareMail(e, recipe.id, recipe.name)}>
+          <label htmlFor="email">Email to:<input textholder="Email" type="email" required id="email" name="email"/></label>
+          <button type="submit">Share by email</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Sharing
