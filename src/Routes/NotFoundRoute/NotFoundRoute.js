76881f3e4214bcf543535e646c5df4b3aaa9f10c
Hector Valles <hmvalles75@yahoo.com>
2019-11-06 12:14:40 -0800

    styled not found page

--- src/Routes/NotFoundRoute/NotFoundRoute.js
+++ src/Routes/NotFoundRoute/NotFoundRoute.js
@@ -8,36 +8,18 @@ class NotFoundRoute extends React.Component {
   render() {
     return(
       <section className="notFound">
-        
         <div className="head">
-          <h2>404</h2>
-          <p>Not Found</p>
+          <h2>4<span>0</span>4</h2>
+          <p>THE PAGE YOU REQUESTED</p>
+          <p>COULD NOT BE FOUND</p>
         </div>
-        
-        <p>Sorry the page you are looking for is not here.</p>
-        
-        <p>Maybe you were looking for one of these?</p>
         
         <div className="links">
           <Link
-            to="/publicrecipes"
+            to="/"
             className="menu">
-            <i className="fas fa-utensil-spoon">
-              <span>{ ' All Recipes' }</span>
-            </i>
-          </Link>
-          <Link
-            to = "/recipes"
-            className = "menu">
-            <i className="fas fa-utensils">
-              <span>{ ' My Recipes' }</span>
-            </i>
-          </Link>
-          <Link
-            to="/login"
-            className="menu">
-            <i className="fas fa-sign-in-alt">
-              <span>{ ' LogIn' }</span>
+            <i className="fas fa-home">
+              <span>{ ' GO TO HOMEPAGE' }</span>
             </i>
           </Link>
         </div>
